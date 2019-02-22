b9f909ed572e5ca25266ac1b901183e8331ab74c
root <Danyllo Silva>
2019-02-22 07:11:20 -0300

    adding database connection with postgresql

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,8 @@ const express = require("express");
 const routes = express.Router();
 
 routes.get("/", (req, res) => {
-  return res.send("Hello World");
+  //return res.send("Hello World");
+  res.render('auth/signup')
 });
 
 module.exports = routes;
